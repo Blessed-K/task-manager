86d5fbefe9576db7d7a022a463aa07bd1ad91ac5
Blessed <kimaniblessed545@gmail.com>
2025-11-03 15:08:08 +0300

    added a timeout for the alerts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,6 +91,18 @@
 
 
     </style>
+
+    <script>
+    setTimeout(() => {
+        const alerts = document.querySelectorAll('.alert');
+        alerts.forEach(alert => {
+            alert.style.transition = 'opacity 0.5s';
+            alert.style.opacity = '0';
+            setTimeout(() => alert.remove(), 500);
+        });
+    }, 3000);
+</script>
+
 </head>
 <body>
     <nav class="navbar navbar-expand-lg">
